c1884032f6e4127288ae9de041958b2f32b7feab
paragspatil <paragspatil123@gmail.com>
2020-06-13 12:34:26 +0530

    Simple banking system stage 4 final commit complete

--- Simple Banking System/task/src/banking/Main.java
+++ Simple Banking System/task/src/banking/Main.java
@@ -117,7 +117,10 @@ public class Main {
 
                         while (loginflag && exitflag) {
                             System.out.println("1. Balance");
-                            System.out.println("2. Log out");
+                            System.out.println("2. Add income");
+                            System.out.println("3. Do transfer");
+                            System.out.println("4. Close account");
+                            System.out.println("5. Log out");
                             System.out.println("0. Exit");
 
                             String userChoice = scanner.nextLine();
@@ -126,8 +129,84 @@ public class Main {
                                 case  "1":
                                   System.out.println("Balance: " + userBalance.get(cardNum));
                                   break;
-                                  //2. Log out
+
+                                  //2. add income
                                 case "2":
+                                    System.out.println("Enter income:");
+                                    int balance = Integer.parseInt(scanner.nextLine());
+                                    userBalance.replace(cardNum,userBalance.get(cardNum) + balance);
+                                    //add user balance to database:
+                                    String insertBalance = "UPDATE cards SET balance = balance + " + balance + "WHERE number =" + cardNum;
+                                    try (Connection con = dataSource.getConnection()) {
+                                        if (con.isValid(5)) {
+                                            //System.out.println("Connection is valid.");
+                                            Statement insert = con.createStatement();
+                                            insert.execute(insertBalance);
+                                        }
+                                    } catch (SQLException e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                // transefer money
+                                case "3":
+                                    System.out.println("Enter card number:");
+                                    long tarnseferCardNum = Long.parseLong(scanner.nextLine());
+                                    if(tarnseferCardNum == cardNum){
+                                       System.out.println("You can't transfer money to the same account!");
+                                    }
+                                    else if(userDetails.containsKey(tarnseferCardNum)){
+                                        System.out.println("Enter how much money you want to transfer:");
+                                        int amount = Integer.parseInt(scanner.nextLine());
+                                        if(userBalance.get(cardNum)<amount){
+                                            System.out.println("Not enough money!");
+                                        }
+                                        else {
+                                            userBalance.replace(cardNum,userBalance.get(cardNum)-amount);
+                                            userBalance.replace(tarnseferCardNum,userDetails.get(tarnseferCardNum) + amount);
+                                            //update balance in database
+                                            String updateBalance1 = "UPDATE cards SET balance = balance - " + amount + " WHERE number =" + cardNum;
+                                            String updateBalance2 = "UPDATE cards SET balance = balance + " + amount + " WHERE number =" + tarnseferCardNum;
+                                            try (Connection con = dataSource.getConnection()) {
+                                                if (con.isValid(5)) {
+                                                    //System.out.println("Connection is valid.");
+                                                    Statement insert1 = con.createStatement();
+                                                    Statement insert2 = con.createStatement();
+                                                    insert1.execute(updateBalance1);
+                                                    insert2.execute(updateBalance2);
+                                                }
+                                            } catch (SQLException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }
+                                    else if(checkflag(tarnseferCardNum)){
+                                        System.out.println("Probably you made mistake in the card number. Please try again!");
+                                    }
+                                    else {
+                                        System.out.println("Such a card does not exist.");
+                                    }
+                                    break;
+
+                                 // delete account
+                                case "4":
+                                    userBalance.remove(cardNum);
+                                    userDetails.remove(cardNum);
+                                    String delete = "DELETE FROM card WHERE number = " + cardNum;
+                                    //delete account from database
+                                    try(Connection con = dataSource.getConnection()){
+                                        if(con.isValid(5)){
+                                            Statement remove = con.createStatement();
+                                            remove.execute(delete);
+                                        }
+                                    }
+                                    catch (SQLException e){
+                                        e.printStackTrace();
+                                    }
+                                    System.out.println("The account has been closed!");
+                                    break;
+
+                                  //5. Log out
+                                case "5":
                                     System.out.println("You have successfully logged out!");
                                     //scanner.nextLine();
                                     loginflag = false;
@@ -235,5 +314,30 @@ public static Long generateCardNumber(){
             System.out.println(e.getMessage());
         }
     }
+    public static boolean checkflag(Long cardNum) {
+        String cardNUmString = cardNum.toString();
+        int ckeckflag = (int) (cardNum % 10);
+        int[] array = new int[15];
+        for (int i = 0; i < cardNUmString.length() - 1; i++) {
+            if (i % 2 == 0) {
+                int temp = cardNUmString.charAt(i) * 2;
+                if (temp > 9) {
+                    temp = temp - 9;
+                    array[i] = temp;
+                } else {
+                    array[i] = temp;
+                }
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        if (ckeckflag == (10 - (sum % 10))) {
+            return  false;
+        }
+        else {
+            return true;
+        }
+    }
 }
-
